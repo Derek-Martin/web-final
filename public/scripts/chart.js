aca9b664432e6f2e2d21bb20d73186987bf2d163
kyler-workman <workmkyler@gmail.com>
2019-08-25 20:39:20 -0600

    prettier

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,46 +1,46 @@
-window.onload = ()=>{
+window.onload = () => {
 
     var qs = JSON.parse(document.getElementById('data').innerText);
-    var charts=[];//this is unused, but it's here for posterity
-    for(var i=0;i<qs.length;i++) charts.push(new Chart(document.getElementById("chart"+i).getContext("2d"),{
-            type:'bar',
-            data:{
-                labels:(why=>{
-                    var becauseICan=[];
-                    why.answers.forEach(e => {
-                        becauseICan.push(e.a)
-                    });
-                    return becauseICan;
-                })(qs[i]),
-                datasets:[{
-                    label:qs[i].question,
-                    data:qs[i].answers.map(a=>a.count),
-                    backgroundColor:[
-                        'rgba(128,128,128,.2)',
-                        'rgba(255,0,255,.2)',
-                        'rgba(255,0,0,.2)',
-                        'rgba(0,0,255,.2)',
-                    ],
-                    borderColor:[
-                        'rgba(128,128,128,1)',
-                        'rgba(255,0,255,1)',
-                        'rgba(255,0,0,1)',
-                        'rgba(0,0,255,1)',
-                    ],
-                    borderWidth:1
+    var charts = [];//this is unused, but it's here for posterity
+    for (var i = 0; i < qs.length; i++) charts.push(new Chart(document.getElementById("chart" + i).getContext("2d"), {
+        type: 'bar',
+        data: {
+            labels: (why => {
+                var becauseICan = [];
+                why.answers.forEach(e => {
+                    becauseICan.push(e.a)
+                });
+                return becauseICan;
+            })(qs[i]),
+            datasets: [{
+                label: qs[i].question,
+                data: qs[i].answers.map(a => a.count),
+                backgroundColor: [
+                    'rgba(128,128,128,.2)',
+                    'rgba(255,0,255,.2)',
+                    'rgba(255,0,0,.2)',
+                    'rgba(0,0,255,.2)',
+                ],
+                borderColor: [
+                    'rgba(128,128,128,1)',
+                    'rgba(255,0,255,1)',
+                    'rgba(255,0,0,1)',
+                    'rgba(0,0,255,1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
             }
+        }
 
-        }));
-    
+    }));
+
 
 }
--- server.js
+++ server.js
@@ -6,122 +6,121 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-app.set('view engine','pug');
+app.set('view engine', 'pug');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://BeatMyBongos:MongosBongos@cluster0-u2tuz.mongodb.net/test?retryWrites=true&w=majority";
 const ObjectId = require('mongodb').ObjectID;
-const client = new MongoClient(uri,{useNewUrlParser : true, useUnifiedTopology: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbName = "webFinal";
 
 var bcrypt = require('bcryptjs');
 
 const session = require('express-session');
 app.use(session({
-    secret:'I code good',
-    cookie:{}
+    secret: 'I code good',
+    cookie: {}
 }));
 
 
 
-app.get('/',async function (req,res){
+app.get('/', async function (req, res) {
     var model = getSessionObject(req);
 
     var questionArray = await getQuestionDataFromMongo();
-    model['questions']= questionArray.questions;
-    res.render('index',model);
+    model['questions'] = questionArray.questions;
+    res.render('index', model);
 });
 
-app.get('/admin', async function(req,res){
+app.get('/admin', async function (req, res) {
     var obj = getSessionObject(req);
     obj['users'] = await getAllUsers();
 
-    if(obj.admin){
-        res.render('admin', obj);    
+    if (obj.admin) {
+        res.render('admin', obj);
     }
-    else{
-        res.redirect('/');     
+    else {
+        res.redirect('/');
     }
 });
 
-app.get('/test',async function(req,res){
+app.get('/test', async function (req, res) {
     res.render('index');
 
 });
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.render('login');
 });
-app.post('/login',async function(req,res){
+app.post('/login', async function (req, res) {
     var user = req.body;
     var other = await getUserByUserName(user.username);
-    if(other == null){
+    if (other == null) {
         res.redirect('/login');
         return;
     }
 
     var valid = bcrypt.compareSync(user.password, other.password);
-    if(valid && other.status=='active'){
+    if (valid && other.status == 'active') {
         req.session.user = other;
         res.redirect('/');
-    }else{
+    } else {
         res.redirect('/login');
     }
 });
 
-app.get('/register',function(req,res){
-    res.render('register',getQuestions());
+app.get('/register', function (req, res) {
+    res.render('register', getQuestions());
 });
-app.post('/register', async function(req,res){
+app.post('/register', async function (req, res) {
     var person = req.body;
     person.status = 'active';
     person.password = bcrypt.hashSync(person.password);
     insertUser(person);
 
-    var answers = [person.answer0,person.answer1,person.answer2];
+    var answers = [person.answer0, person.answer1, person.answer2];
     updateMongoQuestions(answers);
 
-    req.session.user = person;    
+    req.session.user = person;
 
     res.redirect('/');
 });
 
-app.get('/logout', function(req,res){
+app.get('/logout', function (req, res) {
     req.session.destroy();
     res.redirect('/');
 });
 
-app.get('/profile',function(req,res){
+app.get('/profile', function (req, res) {
     var obj = getSessionObject(req);
     obj['user'] = req.session.user;
-    
-    if(!obj.loggedIn){
+
+    if (!obj.loggedIn) {
         res.redirect('/');
         return;
     }
 
-    res.render('profile',obj);
+    res.render('profile', obj);
 });
-app.post('/profile',async function(req,res){
+app.post('/profile', async function (req, res) {
     var user = req.body;
     var ses = req.session.user;
-    var update = {$set: {username: user.username, email: user.email, age: user.age,password: user.password.length>1?bcrypt.hashSync(user.password):ses.password}};
+    var update = { $set: { username: user.username, email: user.email, age: user.age, password: user.password.length > 1 ? bcrypt.hashSync(user.password) : ses.password } };
     await updateUserById(ses._id, update);
     res.redirect('/logout');
 });
 
-var server = app.listen(8080, function(){
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Server running on "+host+":"+port);
+    console.log("Server running on " + host + ":" + port);
 });
 
 
 
-function getSessionObject(req){
+function getSessionObject(req) {
     var object = {};
-    if(req.session == undefined || req.session.user == undefined)
-    {
+    if (req.session == undefined || req.session.user == undefined) {
         object['admin'] = false;
         object['loggedIn'] = false;
         return object;
@@ -132,53 +131,53 @@ function getSessionObject(req){
     object['username'] = user.username;
     return object;
 }
-async function updateMongoQuestions(userSide){
+async function updateMongoQuestions(userSide) {
     var mongoSide = await getQuestionDataFromMongo();
-    
-    for(let i = 0;i<userSide.length;++i){
+
+    for (let i = 0; i < userSide.length; ++i) {
         mongoSide.questions[i].answers[userSide[i]].count++;
     }
 
     await client.connect();
-    await client.db(dbName).collection("questions").update({},mongoSide);
+    await client.db(dbName).collection("questions").update({}, mongoSide);
     client.close();
 }
 //local file only   
-function getQuestions(){
-    var obj = require(__dirname+"/questions.json");
+function getQuestions() {
+    var obj = require(__dirname + "/questions.json");
     return obj;
 }
 //just gets the 'questions.json' in mongo
-async function getQuestionDataFromMongo(){
+async function getQuestionDataFromMongo() {
     await client.connect();
     var questionsData = await client.db(dbName).collection("questions").findOne();
     client.close();
 
     return questionsData;
 }
-async function insertUser(user){
+async function insertUser(user) {
     await client.connect();
     await client.db(dbName).collection("users").insertOne(user);
     client.close();
 }
-async function getUserByUserName(name){
+async function getUserByUserName(name) {
     var user = {};
     await client.connect();
-    await client.db(dbName).collection("users").findOne({ username: name}).then(
-        data => {user = data;},
-        error => {console.log("Request failed: getUserByUserName: "+error);}
+    await client.db(dbName).collection("users").findOne({ username: name }).then(
+        data => { user = data; },
+        error => { console.log("Request failed: getUserByUserName: " + error); }
     );
     client.close();
     return user;
 }
-async function getAllUsers(){
+async function getAllUsers() {
     await client.connect();
     var users = await client.db(dbName).collection("users").find().toArray();
     client.close();
     return users;
 }
-async function updateUserById(id,update){
+async function updateUserById(id, update) {
     await client.connect();
-    await client.db(dbName).collection('users').updateOne({_id: new ObjectId(id)},update);
+    await client.db(dbName).collection('users').updateOne({ _id: new ObjectId(id) }, update);
     client.close();
 }
